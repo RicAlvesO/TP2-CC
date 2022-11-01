ed47af6e4f2496016c0d8a5bf0fcacb8253b07e4
Rodrigo Freitas <a96547@alunos.uminho.pt>
2022-11-01 19:30:25 +0000

    Parsing created

---
+++ parsing.py
@@ -0,0 +1,19 @@
+import argparse
+from ast import main
+
+def parseFile(file):
+    f = open(file, 'r')
+    lines = f.readlines()
+    for line in lines:
+        if line=='\n' or line=='\r' or line[0]=='#':
+            continue
+        print(line)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="the file to parse")
+    args = parser.parse_args()
+    parseFile(args.file)
+
+if __name__ == "__main__":
+    main()
