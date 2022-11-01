77b0c6454790405ce240e32d039a2df9277656aa
Rodrigo Freitas <a96547@alunos.uminho.pt>
2022-11-01 20:23:18 +0000

    Parse with some functions added

--- parsing.py
+++ parsing.py
@@ -5,12 +5,28 @@ def parseFile(file):
     f = open(file, 'r')
     lines = f.readlines()
     for line in lines:
-        if line=='\n' or line=='\r' or line[0]=='#':
+        if line=='\n' or line=='\r' or line[0]=='#' or line.startswith('#'):
             continue
         else:
             list = line.split(' ')
-            print(list)
-
+            list[2] = list[2].replace('\n', '')
+            if list[1]=='DB':
+                #setSP(list[0], list[2])
+                print('DB',list[0],list[2])
+            elif list[1]=='SP':
+                print('DW',list[0],list[2])
+            elif list[1]=='SS':
+                print('DW',list[0],list[2])
+            elif list[1]=='DD':
+                print('DD',list[0],list[2])
+            elif list[1]=='ST':
+                if list[0]=='root':
+                    #stList(list[2])
+                    print('ST',list[0],list[2])
+            elif list[1]=='LG':
+                if list[0]=='all':
+                    #registerActivity(list[2])
+                    print('LG',list[0],list[2])
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="the file to parse")
