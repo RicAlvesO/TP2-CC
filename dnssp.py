747edc82a664f0616d20a4ec073aa375bf1711fd
RicAlvesO <ricaoimp@gmail.com>
2022-12-23 19:09:46 +0000

    Server Update

--- dnssp.py
+++ dnssp.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
+import parser
 import socket
 import sys
 import threading
 import time
 
-import parser
 
 # Classe para representar um Servidor Primário
 class Server:
@@ -18,26 +18,30 @@ class Server:
     # top_servers: Lista dos Servidores de Topo
     # default_ttl: Tempo de validade, tempo máximo em segundos que os dados podem existir na cache do servidor
     # debug: Indica se o servidor se encontra em modo debug ou não
-    def __init__(self, address, port, domain, log_file, database, top_servers, default_ttl, debug):
+    def __init__(self, stype, address, port, domain, log_file, top_servers, default_ttl, debug, database=None,primary_server=None):
         self.address = address
         self.port = port
         self.domain = domain
         self.clients = []
         self.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.tcp_socket.bind((address,port))
-        self.tcp_socket.listen()
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.udp_socket.bind((address,port))
         self.udp_buffer = 1024
         self.log_file = log_file
-        self.database = database
         self.top_servers = top_servers
         self.last_used = time.time()
         self.default_ttl = default_ttl
         self.debug=True
+        if stype=='sp': 
+            self.database = database
+            self.cache = parser.parse_dataFile(database[0])
+            self.tcp_socket.bind((address,port))
+            self.tcp_socket.listen()
+        if stype=='ss':
+            self.primary_server = primary_server
+            self.cache = {}
         if debug=='shy':
             self.debug=False
-        self.cache = parser.parse_dataFile(database[0])
         for domain,log in self.log_file:
             f=open(log,'a+')
             f.close()
@@ -50,8 +54,6 @@ class Server:
     def write_log(self, file, type, endereco, msg):
         named_tuple = time.localtime() # get struct_time
         time_string = time.strftime("[%m/%d/%Y-%H:%M:%S]", named_tuple)
-        port=str(endereco[1])
-        if port=='0':port=''
         message=time_string +' ' + type + ' ' + endereco[0]+ ':' + str(endereco[1]) + ' ' + msg
         if self.debug:
             print(message)
@@ -113,6 +115,32 @@ class Server:
             self.write_log(self.domain, 'EZ',address, 'SP')
             socket.send("-1".encode())
 
+
+    # Método usado para copiar a cache de um servidor para um outro
+    # primary_server: Servidor Primário que terá a cache copiada
+    def receive_cache(self, primary_server):
+        self.tcp_socket.connect(primary_server)
+        self.tcp_socket.sendall(self.domain.encode())
+        size=int(self.tcp_socket.recv(1024).decode())
+        if size==-1:
+            self.write_log(self.domain, 'EZ',primary_server, 'SP')
+            self.tcp_socket.shutdown(socket.SHUT_RDWR)
+            return
+        self.tcp_socket.sendall("OK".encode())
+        while size>0:
+            message=self.tcp_socket.recv(1024).decode()
+            message=message.split(' ')
+            if message[1] in self.cache:
+                self.cache[message[1]].append((message[0],message[2]+' '+message[3]+' '+message[4]))
+            else:
+                self.cache[message[1]]=[(message[0],message[2]+' '+message[3]+' '+message[4])]
+            self.tcp_socket.sendall("OK".encode())
+            size-=1
+        self.tcp_socket.shutdown(socket.SHUT_RDWR)
+        self.tcp_socket.close()
+        self.write_log(self.domain, 'ZT',primary_server, 'SS')
+
+
     # Método usado para buscar as informações de um domínio e com um tipo especifico
     # domain: Domínio usado
     # type: Tipo do valor
@@ -168,9 +196,14 @@ def main(args):
         default_ttl=int(args[3])
     if len(args)>4 and args[4]=="shy":
         debug='shy'
-    server = Server(server_info['DD'][0][0],port, server_info['ADDRESS'],server_info['LG'], server_info['DB'], server_info['ST'], default_ttl, debug)
-    server.write_log('all', 'ST', ('127.0.0.1',0), str(port)+' '+str(default_ttl)+' '+debug)
-    threading.Thread(target=server.accept_ss, args=(server_info['SS'],server_info['ADDRESS']), daemon=True).start()
+    if(server_info['Type']=='SP'):
+        server = Server(server_info['Type'],server_info['DD'][0][0],port, server_info['ADDRESS'],server_info['LG'], server_info['ST'], default_ttl, debug, server_info['DB'])
+        server.write_log('all', 'ST', ('127.0.0.1',0), str(port)+' '+str(default_ttl)+' '+debug)
+        threading.Thread(target=server.accept_ss, args=(server_info['SS'],server_info['ADDRESS']), daemon=True).start()
+    elif(server_info['Type']=='SS'):
+        server = Server(server_info['Type'],server_info['DD'][0][0],port, server_info['ADDRESS'],server_info['LG'], server_info['ST'], default_ttl, debug, server_info['SP'])
+        server.write_log('all', 'ST', ('127.0.0.1',0), str(port)+' '+str(default_ttl)+' '+debug)
+        server.receive_cache(server.primary_server[0])
     server.accept_clients()
 
 
